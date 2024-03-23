5d8ce80502cc3540ae39abb0f18684bdd22c91fc
Adrian Madrid <adrian.madrid@timeworxs.com>
2024-03-23 15:13:42 -0600

    adding env derived message

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,7 +2,7 @@ class HomeController < ApplicationController
   def index
     render json: {
       status: "ok",
-      message: "Hello!"
+      message: ENV.fetch("HELLO_MESSAGE", "Hello!")
     }
   end
 end
